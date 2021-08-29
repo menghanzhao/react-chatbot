7eeeb1017131cdd2b72d51eb99e556fd8ad6f6be
menghanzhao <menghan1@ualberta.ca>
2021-08-29 13:34:04 -0600

    try to make it work

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
     googleProjectID: process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIAGLOGFLOW_LANGUAGE_CODE,
-    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm,'\n'),
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL
 }

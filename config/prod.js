e87c487049d6a238930fc3c897d888b61623bc5c
menghanzhao <menghan1@ualberta.ca>
2021-08-29 12:48:20 -0600

    little change

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
     googleProjectID: process.env.GOOGLE_PROJECT_ID,
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIAGLOGFLOW_LANGUAGE_CODE,
-    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL
 }

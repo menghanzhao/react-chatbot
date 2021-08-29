77d3aeebe1c2d1271317dde1f8aeeb09dd9c0a1f
menghanzhao <menghan1@ualberta.ca>
2021-08-29 00:08:42 -0600

    moving all the chatbot functions in a different module

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,30 +1,10 @@
-const dialogflow = require('dialogflow');
-const config = require('../config/keys');
-
-const sessionClient = new dialogflow.SessionsClient();
-
-const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
-
+const chatbot = require('../chatbot/chatbot');
 module.exports = app => {
     app.get('/', (req, res) => {
         res.send({ 'hello': 'Hannah' });
     });
     app.post('/api/df_text_query', async(req, res) => {
-
-        const request = {
-            session: sessionPath,
-            queryInput: {
-                text: {
-                    // The query to send to the dialogflow agent
-                    text: req.body.text,
-                    // The language used by the client (en-US)
-                    languageCode: config.dialogFlowSessionLanguageCode
-                }
-            }
-        };
-        let responses = await sessionClient
-            .detectIntent(request);
-           
+        let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
         res.send(responses[0].queryResult);
     });
     app.post('/api/df_event_query', (req, res) => {

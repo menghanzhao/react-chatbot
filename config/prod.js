9109643693dc62368b9838fab2cc2c504b10a22f
menghanzhao <menghan1@ualberta.ca>
2021-08-29 12:33:52 -0600

    fixed another error

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,5 @@ module.exports = {
     dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
     dialogFlowSessionLanguageCode: process.env.DIAGLOGFLOW_LANGUAGE_CODE,
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-    googlePrivateKey: PerformanceObserverEntryList.env.GOOGLE_PRIVATE_KEY
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
 }
